0d6994632dcdeb3d14373f9d3ee43f807f7ba434
Alex Chen <112623chen@gmail.com>
2022-10-21 01:57:30 -0400

    refactor(model): removed fit and train code

--- app.py
+++ app.py
@@ -10,31 +10,6 @@ from sklearn.model_selection import train_test_split
 
 
 #helper functions
-def get_df(seg_type): #* Returns a DataFrame of train or test set of images
-    labels = []
-    pixels = []
-    for image_type in list(os.listdir(f"archive/{seg_type}/{seg_type}")):
-        for image in list(os.listdir(f"archive/{seg_type}/{seg_type}/{image_type}")):
-            img = np.array(Image.open(f"archive/{seg_type}/{seg_type}/{image_type}/{image}").convert('L').resize((150, 150))) 
-            img_y = (img.shape[0])
-            img_x = (img.shape[1])
-            img = img.reshape(1,img_y*img_x)
-            pixels.append(img)
-            labels.append(image_type)
-    pixels = np.array(pixels)
-    pixels = np.reshape(pixels, (-1, 150*150))
-    df = pd.DataFrame(pixels)
-    df.columns = [f"{n}" for n in range(150*150)]
-    df['labels'] = labels
-    return df
-
-def run_model(model, df, test_size): #* run and test ML Model
-    X = df.drop("labels", axis=1)
-    y = df["labels"]
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size)
-    model.fit(X_train, y_train)
-    return model
-
 def get_image(uploaded_file):
     st.image(Image.open(uploaded_file))
     img_array = np.array(Image.open(uploaded_file).convert('L').resize((150, 150))).reshape(1,150*150)
@@ -63,13 +38,6 @@ with c2:
     uploaded_file = st.file_uploader("")
     if uploaded_file is not None:
         # ML Model
-        train_df = get_df('seg_train')
-        test_df = get_df('seg_test')
-        df = pd.concat([train_df,test_df])
-        print(round(len(df)/2))
-        df = df.iloc[:round(len(df)/2)] #! could change df memory size up to 200MB
-        # model = run_model(RandomForestClassifier(), df, 0.2) #* Function to Model
-        # pickle.dump(model, open('RandomForest_0.2.pkl', 'wb')) #* Save model as pickle
         model = pickle.load(open('RandomForest_0.2.pkl','rb'))
         #results
         image_type = get_image_type(model, uploaded_file)
